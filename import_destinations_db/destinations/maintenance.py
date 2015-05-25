9f35926a4641418253ef719b6add82f07bc0bce4
ThCP <riccardocppzz93@gmail.com>
2015-05-25 23:46:24 +0200

    added functions

--- import_destinations_db/destinations/maintenance.py
+++ import_destinations_db/destinations/maintenance.py
@@ -7,4 +7,63 @@ This file contains all the update/insert/delete commands
 
 '''
 
+import sqlite3
 
+################# CREATE ENTRIES ####################
+def insert_new_destination (data):
+    
+        
+    sql = '''
+            INSERT INTO destinations 
+            (
+                rm_id, 
+                bl_id,
+                fl_id,
+                lat,
+                lon,
+                name,
+                rm_cat,
+                rm_type,
+                dv_id,
+                dp_id
+            )
+            VALUES
+            (?,?,?,?,?,?,?,?,?,?);
+        '''
+
+    conn = sqlite3.connect("stations.db")
+    c = conn.cursor()
+    c.execute ( sql,
+                (data['bl_id'], 
+                data['fl_id'],
+                data['rm_id'],
+                data['lat'],
+                data['lon'],
+                data['name'],
+                data['rm_cat'],
+                data['rm_type'],
+                data['dv_id'],
+                data['dp_id']
+                ) 
+               )
+    conn.commit()
+    
+    c.close()
+
+################## UPDATE ENTRIES ##################
+def update_destination_data (station_data, station_ID):
+
+    sid = station_data['station_id']
+    sip = station_data['station_ip']
+    lat = station_data['lat']
+    long = station_data['long']
+       
+    sql = 'UPDATE stations SET SID = ?, SIP = ?, LAT = ?, LONG = ? WHERE SID = ?;'   
+        
+    conn = sqlite3.connect("stations.db")
+    c = conn.cursor()
+    c.execute ( sql, (sid, sip, lat, long, station_ID) )
+    conn.commit()
+    print 'updated station %s ' % sid
+    
+    c.close()
--- import_destinations_db/destinations/queries.py
+++ import_destinations_db/destinations/queries.py
@@ -6,3 +6,30 @@ Created on 25/mag/2015
 This file contains all the queries.
 
 '''
+
+import sqlite3
+
+def fetch_destination_data (station_id):
+    sql = 'SELECT * FROM stations WHERE SID = ?;'
+
+    conn = sqlite3.connect("stations.db")
+    c = conn.cursor()
+    c.execute(
+              sql,
+              (station_id,)
+              )
+    result = c.fetchone()
+    c.close()
+    print "station data:"
+    print result
+
+def fetch_all_destinations():
+    sql = 'SELECT * FROM stations'
+    conn = sqlite3.connect("stations.db")
+    c = conn.cursor()
+    c.execute(
+              sql
+              )
+    result = c.fetchall()
+    c.close()
+    return result
